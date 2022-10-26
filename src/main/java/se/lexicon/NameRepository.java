f9f53a0ebe1b825b39d43d1cee256edc794b03af
lubna <lubnafarheensweden@gmail.com>
2022-10-26 14:01:54 +0200

    Tried to simplify the methods in part1

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,51 +1,50 @@
 package se.lexicon;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NameRepository {
-
-    private static String[] names = new String[0];
-
+    private static String[] names = {};
 
     public static int getSize() {
-        int size = names.length;
-        return size;
+        return names.length;
     }
 
     public static void setNames(String[] names) {
-        // todo: Esther
+        NameRepository.names = names;
     }
 
     public static void clear() {
-        // todo: tesfaldet
+        names = new String[]{};
     }
 
     public static String[] findAll() {
-
-        return new String[0];
+        return names;
     }
-    public static String find(final String fullName){
 
+    public static String find(final String fullName) {
+        for (int i = 0; i < fullName.length(); i++) {
+
+        }
         return fullName;
     }
-    public static boolean add(final String fullName){
 
+    public static boolean add(final String fullName) {
         return false;
     }
-    public static String[] findByFirstName(final String firstName){
 
+    public static String[] findByFirstName(final String firstName) {
         return new String[0];
     }
-    public static String[] findByLastName(final String lastName){
 
+    public static String[] findByLastName(final String lastName) {
         return new String[0];
     }
-    public static boolean update(final String original, final String updatedName) {
 
+    public static boolean update(final String original , final String updatedName) {
         return false;
     }
-    public static boolean remove(final String fullName){
 
+    public static boolean remove(final String fullName) {
         return false;
     }
 }
