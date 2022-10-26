074f9c6b872f8cfb8c7bae0773e4e5ac6d7dff49
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2022-10-26 11:58:00 +0200

    workshop

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,4 +1,24 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+
 public class NameRepository {
+
+    private static String[] names = new String[0];
+
+
+    public static int getSize() {
+        int size = names.length;
+        return size;
+    }
+
+    public static void setNames(String[] names) {
+        // todo:
+    }
+
+    public static void clear() {
+        // todo: tesfaldet
+    }
+
+
 }
--- src/test/java/se/lexicon/NameRepositoryTest.java
+++ src/test/java/se/lexicon/NameRepositoryTest.java
@@ -1,6 +1,9 @@
 package se.lexicon;
 
+import org.junit.Before;
+import org.junit.Test;
 
+import org.junit.Assert;
 
 public class NameRepositoryTest {
 
