34f07e5e4f916f0d402287a27a086bb61efb1f25
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2022-10-27 09:12:37 +0200

    addition change are made

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,23 +1,40 @@
 package se.lexicon;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class App {
+    static NameRepository repo = new NameRepository();
+
+
     public static void main(String[] args) {
-        System.out.println(NameRepository.getSize());
-        String [] names = {"Arnold Welsh", "Mikel Peters", "Thomas Kult"};
-        NameRepository.setNames(names);
-        System.out.println(Arrays.toString(names));
-        System.out.println(NameRepository.getSize());
-        //System.out.println(NameRepository.clear(names));
-        System.out.println(NameRepository.find("Arold Welsh"));
-        System.out.println(NameRepository.add("Mikel Pete"));
-        System.out.println(Arrays.toString(NameRepository.findAll()));
-        System.out.println(Arrays.toString(NameRepository.findByFirstName("mikel")));
-        System.out.println(Arrays.toString(NameRepository.findByLastName("peters")));
+        createName();
+        findByFirstName();
 
     }
 
+    public static void createName() {
+        String[] user = new String[]{"anna jao", "tes weld", "fil tek", "arr fan", "tes aron", " tes mel", "tes dan"};
+        repo.setNames(user);
 
+        for (String st: user) {
+            System.out.println(st);
+        }
+    }
+    public static void findByFirstName(){
+        String [] test = repo.findByFirstName("tes");
+        int i = 0;
+        for(String name: test){
+            System.out.println("name # " + ++i + ". " + name);
+        }
+
+    }
+    public static void findByLastName(){
+        String [] test2 = repo.findByLastName("tek");
+        int i = 0;
+        for (String name: test2){
+            System.out.println("lastname = "+ ++i + "." + name);
+        }
+
+    }
 
 }
